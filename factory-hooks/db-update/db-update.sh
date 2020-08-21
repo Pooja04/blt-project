a60a3b548d6da12a13cbc58be5cd3c31a19398ed
vagrant <vagrant@local.my-project.com>
2020-08-21 10:54:43 +0000

    BLT-4: add all the files to test artifacts deployment.

--- factory-hooks/db-update/db-update.sh
+++ factory-hooks/db-update/db-update.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Factory Hook: db-moduleenable
+# Factory Hook: db-update
 #
 # The existence of one or more executable files in the
 # /factory-hooks/db-update directory will prompt them to be run
@@ -8,28 +8,18 @@
 # that update command will normally be part of the commands executed
 # below.
 #
-# Usage: SCRIPTNAME site env db-role domain custom-arg1 custom-arg2 ...
+# Usage: SCRIPTNAME site env db-role domain custom-arg
 # Map the script inputs to convenient names.
 # Acquia hosting site / environment names
 site="$1"
 env="$2"
-
 # database role. (Not expected to be needed in most hook scripts.)
 db_role="$3"
-
 # The public domain name of the website.
 domain="$4"
+# BLT executable:
+blt="/var/www/html/$site.$env/vendor/acquia/blt/blt/bin/blt"
+drush status
+com="$blt deploy:update --define environment=$env --define drush.uri=$domain -v -y"
 
-# Custom argument: we will run entity updates if it is in any way
-# nonempty.
-update_entities="$5"
-
-# The websites' document root can be derived from the site/env:
-docroot="/var/www/html/$site.$env/docroot"
-
-# Acquia recommends the following two practices:
-# 1. Hardcode the drush version.
-# 2. When running drush, provide the application + url, rather than relying
-#    on aliases. This can prevent some hard to trace problems.
-DRUSH_CMD="drush8 --root=$docroot --uri=https://$domain"
-$DRUSH_CMD en shield
+$com
